e44e1f4fe18ffcca13e8dc0f911b2ca1f0d2fc4f
liuhz <liuhuazhong@kylinos.cn>
2022-08-02 18:03:03 +0800

    2 TwoSumLinked

--- src/main/java/org/example/code/TwoSumLinked.java
+++ src/main/java/org/example/code/TwoSumLinked.java
@@ -15,15 +15,18 @@ import java.util.Optional;
 public class TwoSumLinked {
 
     public static void main(String[] args) {
-        Utils.printLinked(sum(Utils.list2Linked(new Integer[]{9,9,9,9,9,9,9}),
-                Utils.list2Linked(new Integer[]{9,9,9,9})));
+//        Utils.printLinked(sum(Utils.list2Linked(new Integer[]{9,9,9,9,9,9,9}),
+//                Utils.list2Linked(new Integer[]{9,9,9,9})));
+//
+        Utils.printLinked(sum(Utils.list2Linked(new Integer[]{2,4,9}),
+                Utils.list2Linked(new Integer[]{5,6,4,9})));
     }
 
     public static ListNode sum(@Nonnull ListNode l1, @Nonnull ListNode l2) {
 
         ListNode h1 = l1;
         ListNode h2 = l2;
-        int remainder = 0, sum = 0;
+        int remainder = 0, sum;
         while (h1 != null && h2 != null) {
             sum = h1.val + h2.val + remainder;
             if (sum > 9) {
@@ -36,7 +39,11 @@ public class TwoSumLinked {
         }
 
 
-        h1 = Optional.ofNullable(h1).orElse(h2);
+        if(h1 == null){
+            h1 = h2;
+            l1 = l2;
+        }
+
         while (h1 != null) {
             sum = h1.val + remainder;
             if (sum > 9) {
@@ -44,12 +51,14 @@ public class TwoSumLinked {
                 sum -= 10;
             }
             h1.val = sum;
-            if (h1.next == null && remainder !=0){
+
+            if (h1.next == null && remainder > 0){
                 h1.next = new ListNode(remainder);
-                sum = 0;
+                remainder = 0;
             }
 
             h1 = h1.next;
+
         }
 
         return l1;
